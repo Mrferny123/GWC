a954f125bc4fa1b07bd55e6b891057447fb44df5
Nissi Sanju <138322675+Mrferny123@users.noreply.github.com>
2025-09-01 22:58:28 +0000

    Cyber security chatbot

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
 #CyHelp Starter Code
+cybersecurityBirthYear = 1970
 
 
 #Greets user
 print("Hello! I'm CyHelp.")
-
+userName = input("What is your name?\n")
+print("Nice to meet you " + userName)
 
 #Recounts start of Cybersecurity
-
+todaysYear = input("What year is it?\n")
+timePassed = int(todaysYear) - cybersecurityBirthYear
+print("Wow! That means it has ben " + str(timePassed) + " years since Cybersecurity began!")
 
 
 print("The field of Cybersecurity started in the 1970s when more and more information started being stored on computer systems and networks!")
+
 input("Press enter to continue.\n")
 
 #Describes Cybersecurity
 print("Cybersecurity refers to the practices that people use to protect computer systems and networks from digital threats.")
-print("These people can be government, nations, companies, community, organizations, and individuals")
+print("These people can be government, nations, companies, community, organizations, and individuals.\n")
 
 
 #Introduces CIA Triad
-print("The CIA Triad is the model used to discuss cybersecurity. CIA stands for (add text here)")
-
+print("The CIA Triad is the model used to discuss cybersecurity. CIA stands for confidentiality, integrity, and availability.")
+print("Would you like to learn about the CIA Triad")
+giveInfo = input("Type 'yes' or 'no'\n")
 
 #Explains pillars of CIA Triad
-print("What would you like to learn more about? Enter the lowercase letter of the following options: (add text here)")
-
+while giveInfo.lower() == "yes":
+  print("What would you like to learn more about? Enter the lowercase letter of the following options: \n(a) confideniality, (b) integrity, (c) availability, or (d) none")
+  
+  topic = input().lower()
+  
+  if topic == 'a':
+    print("Confidentiality makes sure data is private.")
+  elif topic == 'b':
+    print("Integrity makes sure data has not been tampered with and can be trusted.")
+  elif topic == 'c':
+    print("Availability makes sure authorized people can access the data.")
+  elif topic == 'd':
+    print("Okay")
+    break
+  else:
+    print("Sorry, I didn't catch that. Choose one of the options listed.")
 
 #Chatbot ends conversation
 print("Thanks for chatting with me, and I hope you learned something new!")
